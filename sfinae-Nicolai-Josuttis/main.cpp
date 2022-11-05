cdc46ca47ff5ddaf420b4a91293a6a80730a34e6
a.zurov <a.zurov@s-e.su>
2022-11-05 20:45:41 +0300

    Add gitignore, makefile and VS solution

---
+++ sfinae-Nicolai-Josuttis/main.cpp
@@ -0,0 +1,14 @@
+// main.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// ------------------------------------------------------------------------------------------------
+// Inspired by CppCon 2017: Nicolai Josuttis “The Nightmare of Move Semantics for Trivial Classes”:
+// https://www.youtube.com/watch?v=PNRju6_yn3o
+// Presentation Slides, PDFs, Source Code and other presenter materials are available at:
+// https://github.com/CppCon/CppCon2017
+// ------------------------------------------------------------------------------------------------
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
