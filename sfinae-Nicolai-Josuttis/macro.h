a0ecd6c53a2f8edb2eb6be3ffa2c4cbef3bf838d
a.zurov <a.zurov@s-e.su>
2022-11-06 11:47:56 +0300

    added Cxx version constants

--- sfinae-Nicolai-Josuttis/macro.h
+++ sfinae-Nicolai-Josuttis/macro.h
@@ -22,6 +22,7 @@
 #define cout_dump_msg(x)  std::cout << __MAKE_DUMP__ << " " << x << std::endl
 #define checkpoint(x) std::cout << __MAKE_DUMP__ << " Checkpoint#" << #x << std::endl
 
+
 #define BEGIN_NAMESPACE_CCN namespace ccn { // begin check concept namespace
 #define END_NAMESPACE_CCN }                 // end check concept namespace
 
@@ -57,7 +58,6 @@ std::ostream& operator << (std::ostream& os, const conststr& s) {
     return s.m_pBuff ? os.write(s.m_pBuff, s.m_nSize) : os << "\"nullptr\"";
 }
 
-
 /**
 * The macro BOOST_CXX_VERSION is set to the C++ standard version -
 * it has the same value as __cplusplus * if __cplusplus is set to a sensible value,
@@ -66,22 +66,25 @@ std::ostream& operator << (std::ostream& os, const conststr& s) {
 * https://www.boost.org/doc/libs/master/libs/config/doc/html/boost_config/boost_macro_reference.html
 */
 
+#define _Cxx_98 199711L
+#define _Cxx_11 201103L
+#define _Cxx_14 201402L
+#define _Cxx_17 201703L
+#define _Cxx_20 202002L
+
+#define _NAME(x) #x
+
 constexpr conststr getCxxVersionName()
 {
     switch (__CXX_VER__)
     {
-    case 199711L:
-        return "C++98";
-    case 201103L:
-        return "C++11";
-    case 201402L:
-        return "C++14";
-    case 201703L:
-        return "C++17";
-    case 202002L:
-        return "C++20";
+    case _Cxx_98: return _NAME(_Cxx_98);
+    case _Cxx_11: return _NAME(_Cxx_11);
+    case _Cxx_14: return _NAME(_Cxx_14);
+    case _Cxx_17: return _NAME(_Cxx_17);
+    case _Cxx_20: return _NAME(_Cxx_20);
     default:
-        return "C++Unknown";
+        return "unknown Cxx version";
     }
 }
 
