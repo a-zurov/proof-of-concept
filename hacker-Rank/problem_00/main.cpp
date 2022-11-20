1ddf9a8804106d4f941cf4f87f26ee2112d347fa
a.zurov <a.zurov@s-e.su>
2022-11-20 23:16:38 +0200

    environment

--- hacker-Rank/problem_00/main.cpp
+++ hacker-Rank/problem_00/main.cpp
@@ -3,19 +3,67 @@
 
 #include "macro.h"
 
-#include <iostream>
+#include "./../bits/bits-stdc++.h"
+
+using namespace std;
+
+string ltrim(const string&);
+string rtrim(const string&);
+
+/*
+ * Complete the 'fizzBuzz' function below.
+ *
+ * The function accepts INTEGER n as parameter.
+ */
+
+
+void printFizzBuzz(size_t n) {
+
+    if (n % 15 == 0) {
+        cout << "FizzBuzz" << '\n';
+        return;
+    }
+    cout << (n % 3 == 0 ? "Fizz" : (n % 5 == 0 ? "Buzz" : to_string(n))) << '\n';
+}
+
+void fizzBuzz(int n) {
+
+    for (size_t j = 1; j < n + 1; ++j) {
+        printFizzBuzz(j);
+    }
+}
 
 int main()
 {
-    cout_dump_msg("start");
+    string n_temp;
 
-    {
-        dump_duration("cycle");
-        for (int j = 0; j < 1000; j++) {
-            std::cout << '.';
-        }
-    }
+    getline(cin, n_temp);
 
-    cout_dump_msg("stop");
+    int n = stoi(ltrim(rtrim(n_temp)));
 
+    fizzBuzz(n);
+
+    return 0;
+}
+
+string ltrim(const string& str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string& str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
 }
