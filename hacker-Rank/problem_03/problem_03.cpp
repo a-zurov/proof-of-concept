d28679bdf29c21c30e28667f1f55404fdb29f49c
a.zurov <a.zurov@s-e.su>
2022-11-21 22:14:16 +0200

    problem_03

--- hacker-Rank/problem_03/problem_03.cpp
+++ hacker-Rank/problem_03/problem_03.cpp
@@ -8,8 +8,18 @@ using namespace std;
 
 #define INPUT_FROM_FILE
 
+string ltrim(const string&);
+string rtrim(const string&);
+
+// Complete the maxTickets function below.
+int maxTickets(vector<int> tickets) {
+
+    return 0;
+}
+
 int main()
 {
+
 #ifdef INPUT_FROM_FILE
     ifstream input_stream("test.txt");
     if (!input_stream.is_open()) {
@@ -18,13 +28,60 @@ int main()
     }
 #else
 #define input_stream std::cin
+
+    ofstream fout(getenv("OUTPUT_PATH"));
+
 #endif //INPUT_FROM_FILE
+    
 
+    string tickets_count_temp;
+    getline(input_stream, tickets_count_temp);
 
+    int tickets_count = stoi(ltrim(rtrim(tickets_count_temp)));
 
+    vector<int> tickets(tickets_count);
 
+    for (int i = 0; i < tickets_count; i++) {
+        string tickets_item_temp;
+        getline(input_stream, tickets_item_temp);
+
+        int tickets_item = stoi(ltrim(rtrim(tickets_item_temp)));
+
+        tickets[i] = tickets_item;
+    }
+
+    int res = maxTickets(tickets);
+    
 
 #ifdef INPUT_FROM_FILE
+    cout << res << "\n";
     input_stream.close();
+#else
+    fout << res << "\n";
+    fout.close();
 #endif //INPUT_FROM_FILE
+
+    return 0;
+}
+
+string ltrim(const string& str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string& str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
 }
