3549d9c3ecc42696499ac00d15474e90b478a4e9
a.zurov <a.zurov@s-e.su>
2022-11-22 00:26:15 +0200

    minor changes problem_03

--- hacker-Rank/problem_03/problem_03.cpp
+++ hacker-Rank/problem_03/problem_03.cpp
@@ -6,16 +6,35 @@
 
 using namespace std;
 
-#define INPUT_FROM_FILE
-
 string ltrim(const string&);
 string rtrim(const string&);
 
 // Complete the maxTickets function below.
 int maxTickets(vector<int> tickets) {
 
-    return 0;
+    if(0 == tickets.size()) return 0;
+    if(1 == tickets.size()) return 1;
+
+    sort(begin(tickets), end(tickets));
+
+    int nMaxLength = 1;
+    int nCurLength = 1;
+
+    for (int j = 1; j < tickets.size(); ++j) {
+
+        if (tickets[j] - tickets[j - 1] < 2)
+        {
+            ++nCurLength;
+        } else {
+            nMaxLength = max(nMaxLength, nCurLength);
+            nCurLength = 1;
+        }
+    }
+    return nMaxLength;
 }
+
+// comment this macro and check includes before copying to HackerRank editor
+#define INPUT_FROM_FILE
 
 int main()
 {
@@ -28,11 +47,9 @@ int main()
     }
 #else
 #define input_stream std::cin
-
     ofstream fout(getenv("OUTPUT_PATH"));
-
 #endif //INPUT_FROM_FILE
-    
+
 
     string tickets_count_temp;
     getline(input_stream, tickets_count_temp);
@@ -42,6 +59,7 @@ int main()
     vector<int> tickets(tickets_count);
 
     for (int i = 0; i < tickets_count; i++) {
+
         string tickets_item_temp;
         getline(input_stream, tickets_item_temp);
 
@@ -51,7 +69,7 @@ int main()
     }
 
     int res = maxTickets(tickets);
-    
+
 
 #ifdef INPUT_FROM_FILE
     cout << res << "\n";
@@ -66,22 +84,18 @@ int main()
 
 string ltrim(const string& str) {
     string s(str);
-
     s.erase(
         s.begin(),
         find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
     );
-
     return s;
 }
 
 string rtrim(const string& str) {
     string s(str);
-
     s.erase(
         find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
         s.end()
     );
-
     return s;
 }
