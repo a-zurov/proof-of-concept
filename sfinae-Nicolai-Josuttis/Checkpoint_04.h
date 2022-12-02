c97635c61d6840a920ad0eb72be3ff65abdb9d1c
a.zurov <a.zurov@s-e.su>
2022-12-02 04:10:40 +0200

    the main crutch of move semantics

--- sfinae-Nicolai-Josuttis/Checkpoint_04.h
+++ sfinae-Nicolai-Josuttis/Checkpoint_04.h
@@ -3,6 +3,7 @@
 #include <type_traits>
 
 #include "macro.h"
+#include "String.h"
 
 template <typename T>
 typename std::remove_reference<T>::type && move(T&& x) {
@@ -10,10 +11,27 @@ typename std::remove_reference<T>::type && move(T&& x) {
     return static_cast< typename std::remove_reference<T>::type && >(x);
 }
 
+using String = cxx::String;
+
+struct GreedyBox2Strings {
+
+    String  m_szFirst;
+    String  m_szLast;
+
+    template< typename S1
+        , typename S2 = String
+        , typename = std::enable_if_t< std::is_convertible_v<S1, String> >
+    > GreedyBox2Strings(S1&& first, S2&& last = "")
+        : m_szFirst( std::is_same< decltype(first), S1&& >::value ? move(first) : first  )
+        , m_szLast( std::is_same< decltype(last), S2&& >::value ? move(last) : last ) {}
+};
+
 void Checkpoint_04()
 {
     checkpoint(04_Begin);
     {
+        checkpoint(04_A);
+
         int x = 1;
 
         int&& rref_x = move(x);
@@ -29,5 +47,19 @@ void Checkpoint_04()
         ref_rref_rref_x = 0;
 
         std::cout << x << rref_x << ref_rref_x << rref_rref_x << ref_rref_rref_x << '\n';
+
+        checkpoint(04_B_0);
+
+        String str = "cde";
+
+        checkpoint(04_B_1);
+
+        GreedyBox2Strings cbsv("abc", str);
+
+        checkpoint(04_B_2);
+
+        GreedyBox2Strings mbsv("abc", move(str));
+
+        checkpoint(04_End);
     }
 }
