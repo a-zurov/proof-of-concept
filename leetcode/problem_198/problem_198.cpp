7f152e45303a274e15beebe51dd8b1ffc1b384a1
a.zurov <a.zurov@s-e.su>
2022-12-30 18:04:56 +0200

    robbery cycle

--- leetcode/problem_198/problem_198.cpp
+++ leetcode/problem_198/problem_198.cpp
@@ -2,6 +2,7 @@
 // -------------------------------------------------------------------------------------------------
 // Leetcode 198. House Robber
 // https://leetcode.com/problems/house-robber/description/
+// https://leetcode.com/problems/house-robber-ii/description/
 
 #include <iostream>
 #include <vector>
@@ -18,26 +19,60 @@ int rob(vector<int>& nums) {
     vector<int> vecMoneyMaxSolution;
     vecMoneyMaxSolution.resize(N);
 
-    auto getMoneyMax = [&]() -> int {
-        for (int i = 2; i < N; ++i) {
+    auto getMoneyMax = [&](int n) -> int {
+        for (int i = 2; i < n; ++i) {
             vecMoneyMaxSolution[i] =
                 max(vecMoneyMaxSolution[i - 2] + nums[i],
                     vecMoneyMaxSolution[i - 1]);
         }
-        return vecMoneyMaxSolution[N - 1];
+        return vecMoneyMaxSolution[n - 1];
     };
 
     vecMoneyMaxSolution[0] = nums[0];
     vecMoneyMaxSolution[1] = nums[0];
 
-    int begin_first_house_amount = getMoneyMax();
+    int begin_first_house_amount = getMoneyMax(N);
 
     std::fill(vecMoneyMaxSolution.begin(), vecMoneyMaxSolution.end(), 0);
 
     vecMoneyMaxSolution[0] = 0;
     vecMoneyMaxSolution[1] = nums[1];
 
-    int begin_second_house_amount = getMoneyMax();
+    int begin_second_house_amount = getMoneyMax(N);
+
+    return max(begin_first_house_amount, begin_second_house_amount);
+}
+
+int cycle_rob(vector<int>& nums) {
+
+    int N = nums.size();
+    if (!N) return 0;
+    if (1 == N) return nums[0];
+    if (2 == N) return max(nums[0], nums[1]);
+
+    vector<int> vecMoneyMaxSolution;
+    vecMoneyMaxSolution.resize(N);
+
+    auto getMoneyMax = [&](int n) -> int {
+        for (int i = 2; i < n; ++i) {
+            vecMoneyMaxSolution[i] =
+                max(vecMoneyMaxSolution[i - 2] + nums[i],
+                    vecMoneyMaxSolution[i - 1]);
+        }
+        return vecMoneyMaxSolution[n - 1];
+    };
+
+    vecMoneyMaxSolution[0] = nums[0];
+    vecMoneyMaxSolution[1] = nums[0];
+
+    int begin_first_house_amount = getMoneyMax(N - 1);
+
+    std::fill(vecMoneyMaxSolution.begin(), vecMoneyMaxSolution.end(), 0);
+
+    vecMoneyMaxSolution[0] = 0;
+    vecMoneyMaxSolution[1] = nums[1];
+
+    int begin_second_house_amount = getMoneyMax(N);
 
     return max(begin_first_house_amount, begin_second_house_amount);
 }
