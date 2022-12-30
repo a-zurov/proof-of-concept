176a6b80f94170432e610bfb7152a616ad296716
a.zurov <a.zurov@s-e.su>
2022-12-30 17:02:34 +0200

    house robber

---
+++ leetcode/problem_198/problem_198.cpp
@@ -0,0 +1,49 @@
+// problem_198.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// -------------------------------------------------------------------------------------------------
+// Leetcode 198. House Robber
+// https://leetcode.com/problems/house-robber/description/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int rob(vector<int>& nums) {
+
+    int N = nums.size();
+    if (!N) return 0;
+    if (1 == N) return nums[0];
+    if (2 == N) return max(nums[0], nums[1]);
+
+    vector<int> vecMoneyMaxSolution;
+    vecMoneyMaxSolution.resize(N);
+
+    auto getMoneyMax = [&]() -> int {
+        for (int i = 2; i < N; ++i) {
+            vecMoneyMaxSolution[i] =
+                max(vecMoneyMaxSolution[i - 2] + nums[i],
+                    vecMoneyMaxSolution[i - 1]);
+        }
+        return vecMoneyMaxSolution[N - 1];
+    };
+
+    vecMoneyMaxSolution[0] = nums[0];
+    vecMoneyMaxSolution[1] = nums[0];
+
+    int begin_first_house_amount = getMoneyMax();
+
+    std::fill(vecMoneyMaxSolution.begin(), vecMoneyMaxSolution.end(), 0);
+
+    vecMoneyMaxSolution[0] = 0;
+    vecMoneyMaxSolution[1] = nums[1];
+
+    int begin_second_house_amount = getMoneyMax();
+
+    return max(begin_first_house_amount, begin_second_house_amount);
+}
+
+int main()
+{
+    vector<int> nums = { 2,1,1,2 }; // 4
+    std::cout << rob(nums);
+}
