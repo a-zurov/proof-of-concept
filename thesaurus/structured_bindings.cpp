48b603984171f925c73278c006a6d5df215cbb59
a.zurov <a.zurov@s-e.su>
2023-01-21 01:24:25 +0200

    thesaurus init

---
+++ thesaurus/structured_bindings.cpp
@@ -0,0 +1,33 @@
+// structured_bindings.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <map>
+#include <tuple>
+
+int main()
+{
+    std::pair<int, std::string> p1 = { 5, "abc" };
+
+    // since C++17
+    auto [a, b] = p1; // copy
+    std::cout << a << ' ' << b << '\n';
+
+    std::pair<int, std::string> p2 = { 7, "xyz" };
+
+    std::map<int, std::string> m;
+    m.insert(p1);
+    m.insert(p2);
+
+    for (const auto& [key, value] : m) {
+        std::cout << key << ' ' << value << '\n';
+    }
+
+    std::tuple <int, double, std::string> t{ 3, 2.5, "abcdef" };
+    auto& [c, d, e] = t;
+    std::cout << c << ' ' << d << ' ' << e << '\n';
+
+    auto x = std::get<double>(t);
+    auto y = std::get<2>(t);
+    std::cout << x << ' ' << y << '\n';
+}
