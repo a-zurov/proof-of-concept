cc61b314fb881c8b813f4f77596587970aa463c0
a.zurov <a.zurov@s-e.su>
2023-02-09 11:02:03 +0200

    minor changes 2

--- thesaurus/dangling2.cpp
+++ thesaurus/dangling2.cpp
@@ -13,22 +13,34 @@ struct Id {
         return m_id;
     }
 
-    int& better_access() & { // ref-annotation
+    int& better_access() & // ref-annotation
+    {
         return m_id;
     }
+
+    static int m_sid;
+
+    static int& static_access() // ref-qualifiers are not allowed here
+    {
+        return m_sid;
+    }
 };
+
+int Id::m_sid = 1;
 
 int main() {
 
     int& x = Id{}.access(); // dangling reference
     std::cout << x << '\n'; // UB (!)
 
-    // int y = Id{}.better_access(); // CE: a non-const reference may only be bound to an lvalue
-    // const int& z = Id{}.better_access(); // CE: cannot convert 'this' pointer from 'Id' to 'Id &'
+    // int& y = Id{}.better_access(); // CE passing ‘Id’ as ‘this’ argument discards qualifiers
+    // const int& z = Id{}.better_access(); // CE
 
     {
         Id id;
         int& w = id.better_access(); // Ok in this scope
-        std::cout << w;
+        std::cout << w << '\n';
     }
+
+    std::cout << Id::static_access();
 }
