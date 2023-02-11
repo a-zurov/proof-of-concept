3f8199ac480d262908683a6d55b717538a3c2c88
a.zurov <a.zurov@s-e.su>
2023-02-11 22:31:06 +0200

    minor changes

--- thesaurus/best_for.cpp
+++ thesaurus/best_for.cpp
@@ -3,12 +3,40 @@
 
 #include <iostream>
 #include <vector>
+#include <ctime>
+
+struct S {
+    int m_id;
+    std::string m_str;
+};
 
 int main() {
 
-    std::vector<int> v{ 1,2,3 };
+    int count{ 0 };
+
+    clock_t start, fin;
+
+    std::vector<S> v( 1000000, { 1, "abc" } );
+
+    start = clock();
 
     for (auto it = v.begin(), it_end = v.end(); it != it_end; ++it) {
-        std::cout << *it << '\n';
+        if((*it).m_str == "abc") count += (*it).m_id;
     }
+
+    fin = clock();
+
+    std::cout << ( (double)(fin - start) / CLOCKS_PER_SEC ) << '\n';
+
+    count = 0;
+
+    start = clock();
+
+    for (auto it = v.begin(); it != v.end(); ++it) {
+        if ((*it).m_str == "abc") count += (*it).m_id;
+    }
+
+    fin = clock();
+
+    std::cout << ((double)(fin - start) / CLOCKS_PER_SEC);
 }
