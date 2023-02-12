8127139e4acc352cd719ea8e805a2ad84f5cd799
a.zurov <a.zurov@s-e.su>
2023-02-12 17:19:15 +0200

    minor changes

--- thesaurus/avada_kedavra.cpp
+++ thesaurus/avada_kedavra.cpp
@@ -14,24 +14,81 @@
 #define __MAKE_DUMP__ __FILENAME__ << " : " << __LINE__ << " | " << __PRETTY_FUNCTION__
 
 #define cout_dump() std::cout << __MAKE_DUMP__ << '\n'
+#define cout_dump_msg(x) std::cout << __MAKE_DUMP__ << ' ' << x << '\n'
 
-struct S {
-    int m_id{};
-    std::string m_str;
+struct Boolean
+{
+    bool m_bval;
 
-    S operator,(S s)
+    Boolean(bool t_value) : m_bval(t_value) {}
+
+    bool operator&&(const Boolean& right) const
     {
         cout_dump();
+        return this->m_bval && right.m_bval;
+    }
+
+    bool operator||(const Boolean& right) const
+    {
+        cout_dump();
+        return this->m_bval || right.m_bval;
+    }
+
+    operator bool() const
+    {
+        cout_dump();
+        return m_bval;
+    }
+};
+
+bool getbool(bool b) {
+    cout_dump_msg(std::boolalpha << b);
+    return b;
+}
+
+Boolean getBoolean(bool b) {
+    cout_dump_msg(std::boolalpha << b);
+    return Boolean(b);
+}
+
+struct S {
+    bool m_bval{false};
+    std::string m_str;
+
+    S operator,(S s) const
+    {
+        cout_dump_msg(m_str);
         S tmp;
-        tmp.m_id = s.m_id;
+        tmp.m_bval = s.m_bval;
         tmp.m_str = s.m_str;
         return tmp;
+    }
+
+    Boolean operator&&(const S& rhs) const
+    {
+        cout_dump_msg(m_str);
+        return m_bval && rhs.m_bval;
+    }
+
+    operator Boolean() const
+    {
+        cout_dump_msg(m_str);
+        return m_bval;
     }
 };
 
 int main() {
 
-    S s1{ 1,"abc" }, s2{ 2,"xyz" }, s3;
+    cout_dump_msg("test bool operator(&&) expecting 1 getbool(bool) call:");
+    if (getbool(false) && getbool(true)) cout_dump();
+
+    cout_dump_msg("test Boolean operator(&&) expecting 2 getBoolean(bool) calls:");
+    if (getBoolean(false) && getBoolean(true)) cout_dump();
+
+    cout_dump_msg("test operator(,)(S):");
+    S s1{ false, "abc" }, s2{ true, "xyz" }, s3;
     s3 = (s1, s2);
-    std::cout << s3.m_id << ' ' << s3.m_str;
+    std::cout << std::boolalpha << s3.m_bval << ' ' << s3.m_str << '\n';
+
+    if (s2 && s1) cout_dump();
 }
