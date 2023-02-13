2850cecac2ab3e5d43577bbf38095e21620726c6
a.zurov <a.zurov@s-e.su>
2023-02-13 15:29:20 +0200

    init

---
+++ thesaurus/slicing.cpp
@@ -0,0 +1,77 @@
+// slicing.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// ------------------------------------------
+// Slicing with arguments
+
+#include <iostream>
+#include <cstring>
+
+#ifdef _MSC_VER
+#define __PRETTY_FUNCTION__ __FUNCSIG__
+#define __DELIM__ '\\'
+#else
+#define __DELIM__ '/'
+#endif
+#define __FILENAME__ ( std::strrchr( "/" __FILE__, __DELIM__ ) + 1 )
+#define __MAKE_DUMP__ __FILENAME__ << " : " << __LINE__ << " | " << __PRETTY_FUNCTION__
+
+#define LBRACE__ "\n {\n"
+#define __RBRACE "\n }\n"
+
+#define cout_dump() std::cout << __MAKE_DUMP__ << '\n'
+#define cout_dump_msg(x) std::cout << __MAKE_DUMP__ << ' ' << x << '\n'
+
+struct Base {
+
+    int b_;
+
+    virtual ~Base() {}
+
+    Base(int j) : b_(j) {
+        cout_dump_msg(b_);
+    }
+    virtual void dump(std::ostream& os) const {
+        os << __MAKE_DUMP__ << "\n Base dump: " << b_;
+    }
+};
+
+struct Derived : public Base {
+
+    int d_;
+
+    Derived(int j) : Base(j/2), d_(j) {
+        cout_dump_msg(b_ << ' ' << d_);
+    }
+    void dump(std::ostream& os) const override {
+        os << __MAKE_DUMP__ << "\n Derived dump: " << b_ << " " << d_;
+    }
+};
+
+std::ostream& operator<< (std::ostream & os, const Base & base) {
+    //cout_dump();
+    base.dump(os);
+    return os;
+}
+
+void foo(Base base) {
+    std::cout << __MAKE_DUMP__
+        << LBRACE__ << base << __RBRACE;
+}
+
+void bar(Base & base) {
+    std::cout << __MAKE_DUMP__
+        << LBRACE__ << base << __RBRACE;
+}
+
+void par(Base* ptr) {
+    std::cout << __MAKE_DUMP__
+        << LBRACE__ << *ptr << __RBRACE;
+}
+
+int main() {
+
+    Derived d(10);
+
+    foo(d);
+    bar(d);
+    par(&d);
+}
