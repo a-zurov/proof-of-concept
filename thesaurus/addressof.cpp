b4c1d97eef8648673e9f5d24f905f04ee072f811
a.zurov <a.zurov@s-e.su>
2023-03-02 00:16:05 +0200

    init

---
+++ thesaurus/addressof.cpp
@@ -0,0 +1,79 @@
+// addressof.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// --------------------------------------------------------------
+//
+
+#include <iostream>
+#include <cstring>
+
+#if defined( _MSC_VER )
+#define __PRETTY_FUNCTION__ __FUNCSIG__
+#define __DELIM__ '\\'
+#else
+#define __DELIM__ '/'
+#endif
+#define __FILENAME__ ( std::strrchr( "/" __FILE__, __DELIM__ ) + 1 )
+#define __MAKE_DUMP__ __FILENAME__ << " : " << __LINE__ << " | " << __PRETTY_FUNCTION__
+
+#define cout_dump_msg(x) std::cout << __MAKE_DUMP__ << ' ' << x << '\n'
+
+#define ALLOW_DUMP
+#ifdef ALLOW_DUMP
+#define cout_dump() std::cout << __MAKE_DUMP__ << '\n'
+#else
+#define cout_dump()
+#endif
+
+
+template<typename T>
+struct A
+{
+    T* m_ptr;
+
+    A(T& t) : m_ptr(&t) {
+        cout_dump();
+    }
+
+    A(const A<T>& other) : m_ptr(other.m_ptr) {
+        cout_dump();
+    }
+
+    A& operator=(const A<T>& rhs) {
+        cout_dump();
+        m_ptr = rhs.m_ptr;
+        return *this;
+    }
+};
+
+template<typename T>
+struct B {
+
+    T* m_ptr;
+
+    B(T& t) : m_ptr(&t) {
+        cout_dump();
+    }
+
+    B(const B<T>& other) : m_ptr(other.m_ptr) {
+        cout_dump();
+    }
+
+    B& operator=(const B<T>& rhs) {
+        cout_dump();
+        m_ptr = rhs.m_ptr;
+        return *this;
+    }
+};
+
+int main(int argc, char** argv) {
+
+    int x = 5;
+
+#ifdef _MSC_VER
+    auto a = A(x);
+    auto b = B(x);
+#else
+    auto a = A<int>(x);
+    auto b = B<int>(x);
+#endif
+
+}
