67112e2062c14db9f82bc6fe5f7552f4c392fa69
a.zurov <a.zurov@s-e.su>
2023-03-02 18:41:27 +0200

    minor changes 3

--- thesaurus/reference_wrapper.cpp
+++ thesaurus/reference_wrapper.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <cstring>
 #include <cassert>
+#include <ctime>
 #include <vector>
 #include <algorithm>
 
@@ -19,7 +20,7 @@
 
 #define cout_dump_msg(x) std::cout << __MAKE_DUMP__ << ' ' << x << '\n'
 
-#define ALLOW_DUMP
+//#define ALLOW_DUMP
 #ifdef ALLOW_DUMP
 #define cout_dump() std::cout << __MAKE_DUMP__ << '\n'
 #else
@@ -98,10 +99,10 @@ struct B {
 template <typename T>
 struct type_cast_predicate
 {
-    static int m_count;
+    static int ms_fcall_count;
 
     static bool predicate(const T& lhs, const T& rhs) {
-        ++m_count;
+        ++ms_fcall_count;
         return lhs > rhs;
     }
 
@@ -113,58 +114,113 @@ struct type_cast_predicate
 };
 
 template <typename T>
-int type_cast_predicate<T>::m_count = 0;
+int type_cast_predicate<T>::ms_fcall_count = 0;
 
 template <typename T>
 struct functor_predicate
 {
-    int m_count{};
+    static int ms_ctor_count;
+    static int ms_cpytor_count;
+    static int ms_assop_count;
+    static int ms_fcall_count;
+
+    int m_fcall_count{};
+
+    functor_predicate() {
+        cout_dump();
+        ++ms_ctor_count;
+    }
+
+    functor_predicate(const functor_predicate<T>& other) {
+        cout_dump();
+        ++ms_cpytor_count;
+    }
+
+    functor_predicate& operator=(const functor_predicate<T>& rhs) {
+        cout_dump();
+        ++ms_assop_count;
+        return *this;
+    }
 
     bool operator()(const T& lhs, const T& rhs) {
-        ++m_count;
+        ++m_fcall_count;
+        ++ms_fcall_count;
         return lhs > rhs;
     }
 };
+
+template <typename T>
+int functor_predicate<T>::ms_ctor_count = 0;
+
+template <typename T>
+int functor_predicate<T>::ms_cpytor_count = 0;
+
+template <typename T>
+int functor_predicate<T>::ms_assop_count = 0;
+
+template <typename T>
+int functor_predicate<T>::ms_fcall_count = 0;
 
 template <typename T>
 using vector = std::vector<T>;
 
 int main(int argc, char** argv) {
 
-    int x = 5;
-
     vector<int> vec = { 7, 3, 8, 5, 4, 6, 1, 9, 2 };
 
+    if (argc != 2) {
+        printf("can use input parameter\t: %s <number-of-elements>\n",
+            std::strrchr(argv[0], __DELIM__) + 1);
+    } else {
+        size_t N = strtol(argv[1], NULL, 10);
+        assert(N > 0);
+        vec.resize(N);
+        for (size_t j = 0; j < N; ++j) {
+            vec[j] = rand() % N;
+        }
+    }
+
+    clock_t start, finish;
+
     vector<reference_wrapper<int>> vec_rw0{ vec.begin(), vec.end() };
+
+    start = clock();
     std::sort(vec_rw0.begin(), vec_rw0.end(), type_cast_predicate<int>());
-    cout_dump_msg(type_cast_predicate<int>::m_count);
+    finish = clock();
+    cout_dump_msg(type_cast_predicate<int>::ms_fcall_count << " time = " << (double)(finish - start) / CLOCKS_PER_SEC);
 
     functor_predicate<int> fpred;
 
     vector<reference_wrapper<int>> vec_rw1{ vec.begin(), vec.end() };
+
+    start = clock();
     std::sort(vec_rw1.begin(), vec_rw1.end(), fpred);
-    assert(0 == fpred.m_count);
+    finish = clock();
+    cout_dump_msg(fpred.ms_fcall_count << " time = " << (double)(finish - start) / CLOCKS_PER_SEC );
+    assert(0 == fpred.m_fcall_count);
+
+    vector<reference_wrapper<int>> vec_rw2{ vec.begin(), vec.end() };
+
+    start = clock();
+#ifdef _MSC_VER
+    std::sort(vec_rw2.begin(), vec_rw2.end(), reference_wrapper(fpred));
+#else
+    std::sort(vec_rw2.begin(), vec_rw2.end(), reference_wrapper<functor_predicate<int>>(fpred));
+#endif
+    finish = clock();
+    cout_dump_msg(fpred.m_fcall_count << " time = " << (double)(finish - start) / CLOCKS_PER_SEC);
+    assert(type_cast_predicate<int>::ms_fcall_count == fpred.m_fcall_count);
+
+    int x = 5;
 
 #ifdef _MSC_VER
-
     auto a = reference_wrapper(x);
     auto b = B(x);
     auto c = reference_wrapper(b);
-
-    vector<reference_wrapper<int>> vec_rw2{ vec.begin(), vec.end() };
-    std::sort(vec_rw2.begin(), vec_rw2.end(), reference_wrapper(fpred));
-    assert(type_cast_predicate<int>::m_count == fpred.m_count);
-
 #else
-
     auto a = reference_wrapper<int>(x);
     auto b = B<int>(x);
     auto c = reference_wrapper<B<int>>(b);
-
-    vector<reference_wrapper<int>> vec_rw2{ vec.begin(), vec.end() };
-    std::sort(vec_rw2.begin(), vec_rw2.end(), reference_wrapper<functor_predicate<int>>(fpred));
-    assert(type_cast_predicate<int>::m_count == fpred.m_count);
-
 #endif
 
 }
