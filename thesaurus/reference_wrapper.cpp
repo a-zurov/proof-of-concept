acbfefb7c264f76321feae65336c07774c0497fc
a.zurov <a.zurov@s-e.su>
2023-03-02 04:17:39 +0200

    init

---
+++ thesaurus/reference_wrapper.cpp
@@ -0,0 +1,93 @@
+// reference_wrapper.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// --------------------------------------------------------------
+//
+
+#include <iostream>
+#include <cstring>
+
+#if defined( _MSC_VER )
+#define __PRETTY_FUNCTION__ __FUNCSIG__
+#define __DELIM__ '\\'
+#else
+#define __DELIM__ '/'
+#endif
+#define __FILENAME__ ( std::strrchr( "/" __FILE__, __DELIM__ ) + 1 )
+#define __MAKE_DUMP__ __FILENAME__ << " : " << __LINE__ << " | " << __PRETTY_FUNCTION__
+
+#define cout_dump_msg(x) std::cout << __MAKE_DUMP__ << ' ' << x << '\n'
+
+#define ALLOW_DUMP
+#ifdef ALLOW_DUMP
+#define cout_dump() std::cout << __MAKE_DUMP__ << '\n'
+#else
+#define cout_dump()
+#endif
+
+template<class T>
+T* addressof(T& arg)
+{
+    return reinterpret_cast<T*>(
+        &const_cast<char&>(reinterpret_cast<const volatile char&>(arg))
+        );
+}
+
+template<typename T>
+struct reference_wrapper
+{
+    T* m_ptr;
+
+    reference_wrapper(T& t) : m_ptr(addressof(t)) {
+        cout_dump();
+    }
+
+    reference_wrapper(const reference_wrapper<T>& other) : m_ptr(other.m_ptr) {
+        cout_dump();
+    }
+
+    reference_wrapper& operator=(const reference_wrapper<T>& rhs) {
+        cout_dump();
+        m_ptr = rhs.m_ptr;
+        return *this;
+    }
+};
+
+template<typename T>
+struct B {
+
+    T* m_ptr;
+
+    B(T& t) : m_ptr(&t) {
+        cout_dump();
+    }
+
+    B(const B<T>& other) : m_ptr(other.m_ptr) {
+        cout_dump();
+    }
+
+    B& operator=(const B<T>& rhs) {
+        cout_dump();
+        m_ptr = rhs.m_ptr;
+        return *this;
+    }
+
+    T* operator &() const {
+        cout_dump();
+        return m_ptr;
+    }
+};
+
+int main(int argc, char** argv) {
+
+    int x = 5;
+
+#ifdef _MSC_VER
+    auto a = reference_wrapper(x);
+    auto b = B(x);
+    auto c = reference_wrapper(b);
+#else
+    auto a = reference_wrapper<int>(x);
+    auto b = B<int>(x);
+    auto c = reference_wrapper<B<int>>(b);
+#endif
+
+}
