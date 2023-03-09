9c899bdcc08435db566465e8a7a3cd6022201998
a.zurov <a.zurov@s-e.su>
2023-03-09 02:18:46 +0200

    bind minor changes

--- thesaurus/bind.cpp
+++ thesaurus/bind.cpp
@@ -7,6 +7,7 @@
 #include <type_traits>
 #include <functional>
 #include <tuple>
+#include <vector>
 
 #if defined( _MSC_VER )
 #define __PRETTY_FUNCTION__ __FUNCSIG__
@@ -30,41 +31,44 @@
 template<class... _Types>
 struct argument_list
 {
-    std::tuple<_Types...> tupleArgs_;
+    std::tuple<_Types...> tuple_Args_;
 
     template<class... T>
     argument_list(T&&... args) noexcept
-        : tupleArgs_{ std::forward<T>(args)... }
+        : tuple_Args_{ std::forward<T>(args)... }
     {
         cout_dump();
     }
 
     template<size_t n>
-    decltype(auto) operator[](std::integral_constant<size_t, n>) noexcept {
+    decltype(auto) operator[](std::integral_constant<size_t, n>) noexcept
+    {
         cout_dump();
-        return std::get<n>(tupleArgs_);
+        return std::get<n>(tuple_Args_);
     }
 };
 
 template<class... _Types>
-struct callee_list
+struct rref_arg_list
 {
-    std::tuple<_Types&&...> tupleArgs_;
+    std::tuple<_Types&&...> tuple_rref_Args_;
 
     template<class... _TyArgs>
-    callee_list(_TyArgs&&... args) noexcept
-        : tupleArgs_( std::forward<_TyArgs>(args)... )
+    rref_arg_list(_TyArgs&&... args) noexcept
+        : tuple_rref_Args_( std::forward<_TyArgs>(args)...)
     {
         cout_dump();
     }
 
     template< class T,
-        typename = std::enable_if_t< (std::is_placeholder<std::remove_reference_t<T>>::value != 0) >
+        typename = std::enable_if_t<
+        (std::is_placeholder<std::remove_reference_t<T>>::value != 0)
+        >
     >
     decltype(auto) operator[](T) noexcept
     {
         cout_dump();
-        return std::get<std::is_placeholder<T>::value - 1>(std::move(tupleArgs_));
+        return std::get<std::is_placeholder<T>::value - 1>(/*NB(!)*/ std::move(tuple_rref_Args_));
     }
 
     template < typename T
@@ -72,7 +76,7 @@ struct callee_list
             (std::is_placeholder<std::remove_reference_t<T>>::value == 0)
         >
     >
-    decltype(auto) operator[](T&& t) noexcept
+    T&& operator[](T&& t) noexcept // forward
     {
         cout_dump();
         return static_cast<T&&>(t);
@@ -98,7 +102,7 @@ struct binder
     decltype(auto) operator()(_TyArgs&&... args)
     {
         cout_dump();
-        return call( std::make_index_sequence<sizeof...(_Types)>{}, static_cast<_TyArgs&&>(args)...);
+        return call( std::make_index_sequence<sizeof...(_Types)>{}, std::forward<_TyArgs>(args)...);
     }
 
 private:
@@ -109,18 +113,40 @@ private:
         cout_dump();
         return f_(
                     (
-                        callee_list<_TyArgs...>{ static_cast<_TyArgs&&>(args)...} [
+                        rref_arg_list<_TyArgs...>{/*NB(!)*/ std::move(args)... }.operator[] (
                             argumentList_[ std::integral_constant<size_t, Seq>{} ]
-                        ]
+                            )
                     )...
                 );
     }
 };
 
 template<class F, class ... _Types>
-binder<F, _Types...> bind2(F&& f, _Types&&... args)
+binder<F, _Types...> bind2(F f, _Types&&... args)
 {
-    return binder<F, _Types...>{ static_cast<F&&>(f), static_cast<_Types&&>(args)...};
+    return binder<F, _Types...>{ f, std::forward<_Types>(args)...};
+}
+
+struct S {
+    std::string s_;
+    S() { cout_dump(); }
+    ~S() { cout_dump(); }
+    S(const S& other) : s_(other.s_) {
+        cout_dump();
+    }
+    S(S&& other) noexcept : s_(std::move(other.s_))  {
+        cout_dump();
+    }
+};
+
+template<typename T>
+T foo(T&& x, const std::string& s) {
+
+    if (std::is_same<T, S>::value) {
+        cout_dump_msg(s);
+        x.s_ = s;
+    } else cout_dump();
+    return x;
 }
 
 void foo1(int j) {
@@ -138,6 +164,21 @@ int sum(int a, int b) {
 using namespace std::placeholders;
 
 int main(int argc, char** argv) {
+
+    std::vector<S> v;
+
+    auto g = std::bind(foo<S>, _1, "abc");
+    v.emplace_back(std::move(g(S{})));
+
+    auto f = bind2(foo<S>, _1, "xyz");
+    S s;
+    f(std::move(s));
+    v.emplace_back(std::move(f(S{})));
+    v.emplace_back(std::move(f(S{})));
+
+    for (const auto& elem : v) {
+        cout_dump_msg(elem.s_);
+    }
 
     bind2(foo1, 1)();
 
