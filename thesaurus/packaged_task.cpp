a49e634354d60ada0a9e8a65b9cf93702e271e2c
a.zurov <a.zurov@s-e.su>
2023-03-15 02:27:48 +0200

    packaged task minor changes 2

--- thesaurus/packaged_task.cpp
+++ thesaurus/packaged_task.cpp
@@ -48,7 +48,50 @@ auto bar(F&& f, _TyArgs&&... args) -> std::future<
 
 int main()
 {
-    auto x = foo();
-    auto y = bar(sum, 2, 11);
-    std::cout << x.get() << ' ' << y.get() << '\n';
+    auto future_foo = foo();
+    auto future_bar = bar(sum, 2, 11);
+    std::cout << future_foo.get() << ' ' << future_bar.get() << '\n';
+
+    std::promise<int> promise1;
+    auto future1 = promise1.get_future();
+    std::thread th1(
+        [&promise1](int j, int k) {
+            promise1.set_value( sum(j,k) );
+        }
+        , 2, 11 );
+
+    std::promise<int> promise2;
+    auto future2 = promise2.get_future();
+    std::thread th2(
+        [](std::promise<int>&& promise, int j, int k) {
+            promise.set_value(sum(j, k));
+        }
+    , std::move(promise2), 2, 11);
+
+    std::promise<int> promise3;
+    auto future3 = promise3.get_future();
+    std::thread th3(
+        [](std::promise<int>& promise, int j, int k) {
+            promise.set_value(sum(j, k));
+        }
+    , std::ref(promise3), 2, 11);
+
+    std::promise<int> promise4;
+    auto future4 = promise4.get_future();
+    std::thread th4(
+        [promise = std::move(promise4)](int j, int k) mutable {
+            promise.set_value(std::move(sum(j, k)));
+        }
+    , 2, 11);
+
+    std::cout << future1.get() << ' '
+        << future2.get() << ' '
+        << future3.get() << ' '
+        << future4.get() << ' '
+        << '\n';
+
+    th1.join();
+    th2.join();
+    th3.join();
+    th4.join();
 }
