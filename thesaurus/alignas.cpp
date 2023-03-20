b76be5914d68834759c3793bf157da85903818c3
a.zurov <a.zurov@s-e.su>
2023-03-20 18:07:36 +0200

    alignas minor changes 2

--- thesaurus/alignas.cpp
+++ thesaurus/alignas.cpp
@@ -1,11 +1,13 @@
 // alignas.cpp : This file contains the 'main' function. Program execution begins and ends there.
 // --------------------------------------------------------------
 // g++ -pthread alignas.cpp
+// https://rigtorp.se/ringbuffer/
 
 #include <iostream>
 #include <cstring>
 #include <ctime>
 #include <cassert>
+#include <cstddef>
 #include <atomic>
 #include <thread>
 #include <vector>
@@ -66,7 +68,97 @@ struct ShardedCounter {
     }
 };
 
+// ---------------------------------------------
+// Single-Producer Single-Consumer Ring Buffer
+
+template <typename T>
+struct SPSCRingBuffer {
+
+    struct
+#ifdef __ALIGNAS_AVOID_FALSE_SHARING__
+        alignas(__hardware_destructive_interference_size__)
+#endif
+    Slot {
+        T value;
+    };
+
+    std::vector<Slot> buffer_;
+
+#ifdef __ALIGNAS_AVOID_FALSE_SHARING__
+    char padding1_[__hardware_destructive_interference_size__]{};
+#endif
+
+    std::atomic<size_t> head_{ 0 };
+
+#ifdef __ALIGNAS_AVOID_FALSE_SHARING__
+    char padding2_[__hardware_destructive_interference_size__]{};
+#endif
+
+    std::atomic<size_t> tail_{ 0 };
+
+    explicit SPSCRingBuffer(const size_t capacity) : buffer_(capacity + 1) {}
+
+    bool TryProduce(T value) {
+
+        const size_t curr_tail = tail_.load();
+        const size_t curr_head = head_.load();
+
+        if (IsFull(curr_head, curr_tail)) {
+            return false;
+        }
+
+        buffer_[curr_tail].value = std::move(value);
+        tail_.store(Next(curr_tail));
+        return true;
+    }
+
+    bool TryConsume(T& value) {
+
+        const size_t curr_head = head_.load();
+        const size_t curr_tail = tail_.load();
+
+        if (IsEmpty(curr_head, curr_tail)) {
+            return false;
+        }
+
+        value = std::move(buffer_[curr_head].value);
+        head_.store(Next(curr_head));
+        return true;
+    }
+
+private:
+    bool IsFull(size_t head, size_t tail) const {
+        return Next(tail) == head;
+    }
+
+    bool IsEmpty(size_t head, size_t tail) const {
+        return tail == head;
+    }
+
+    size_t Next(size_t slot) const {
+        return (slot + 1) % buffer_.size();
+    }
+};
+
+class Digest {
+public:
+    void Feed(int value) {
+        digest_ = std::hash<int>()(value) ^ (digest_ << 1);
+    }
+
+    size_t Value() const {
+        return digest_;
+    }
+
+private:
+    size_t digest_ = 0;
+};
+
+
 int main() {
+
+    static const int nTries = 3;
+    static const int nValues = 10'000'000;
 
     printf("sizeof(ShardedCounter::AtomicCounter) = %zu\n", sizeof(ShardedCounter::AtomicCounter));
 
@@ -76,18 +168,66 @@ int main() {
 
     clock_t start, finish;
     ShardedCounter counter;
-    start = clock();
-    std::vector<std::thread> vecThreads;
-    for (int i = 0; i < g_nThreads; ++i) {
-        vecThreads.emplace_back([&counter, i]() {
-            for (size_t j = 0; j < 10'000'000; ++j) {
-                counter.Increment(i);
+
+    for (int j = 0; j < nTries; ++j) {
+
+        start = clock();
+        std::vector<std::thread> vecThreads;
+        for (int i = 0; i < g_nThreads; ++i) {
+            vecThreads.emplace_back([&counter, i]() {
+                for (size_t j = 0; j < nValues; ++j) {
+                    counter.Increment(i);
+                }
+                });
+        }
+        for (auto& t : vecThreads) {
+            t.join();
+        }
+        finish = clock();
+        printf("%zu time = %f\n", counter.Get(), (double)(finish - start) / CLOCKS_PER_SEC);
+    }
+
+    // ---------------------------------------------
+    // Single-Producer Single-Consumer Ring Buffer
+
+    printf("sizeof(SPSCRingBuffer<int>) = %zu\n", sizeof(SPSCRingBuffer<int>));
+    printf("alignof(SPSCRingBuffer<int>) = %zu\n", alignof(SPSCRingBuffer<int>));
+#ifdef __ALIGNAS_AVOID_FALSE_SHARING__
+    printf("offsetof(SPSCRingBuffer<int>, padding1_) = %zu\n", offsetof(SPSCRingBuffer<int>, padding1_));
+    printf("sizeof(SPSCRingBuffer<int>::padding1_) = %zu\n", sizeof(SPSCRingBuffer<int>::padding1_));
+#endif
+    printf("offsetof(SPSCRingBuffer<int>, head_) = %zu\n", offsetof(SPSCRingBuffer<int>, head_));
+#ifdef __ALIGNAS_AVOID_FALSE_SHARING__
+    printf("offsetof(SPSCRingBuffer<int>, padding2_) = %zu\n", offsetof(SPSCRingBuffer<int>, padding2_));
+    printf("sizeof(SPSCRingBuffer<int>::padding2_) = %zu\n", sizeof(SPSCRingBuffer<int>::padding2_));
+#endif
+    printf("offsetof(SPSCRingBuffer<int>, tail_) = %zu\n", offsetof(SPSCRingBuffer<int>, tail_));
+
+    SPSCRingBuffer<int> buffer(1024);
+
+    for (int j = 0; j < nTries; ++j) {
+
+        start = clock();
+        std::thread producer([&buffer]() {
+            for (int i = 0; i < nValues; ++i) {
+                while (!buffer.TryProduce(i)) {
+                    std::this_thread::yield();
+                }
             }
-        });
+            });
+        Digest digest;
+        std::thread consumer([&]() {
+            for (int i = 0; i < nValues; ++i) {
+                int value;
+                while (!buffer.TryConsume(value)) {
+                    std::this_thread::yield();
+                }
+                digest.Feed(value);
+            }
+            });
+        producer.join();
+        consumer.join();
+        finish = clock();
+        printf("%zu time = %f\n", digest.Value(), (double)(finish - start) / CLOCKS_PER_SEC);
     }
-    for (auto& t : vecThreads) {
-        t.join();
-    }
-    finish = clock();
-    printf("%zu time = %f\n", counter.Get(), (double)(finish - start) / CLOCKS_PER_SEC);
 }
