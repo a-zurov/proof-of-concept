40146ca337e6a7cb77ca1d5020d9e3390d037cba
a.zurov <a.zurov@s-e.su>
2023-03-24 20:49:19 +0200

    step 2

--- thesaurus/message_passing.cpp
+++ thesaurus/message_passing.cpp
@@ -8,5 +8,30 @@ static const int g_nTries = 1, g_nSignals = 100;
 
 int main()
 {
+    int a = 0;
+
+    std::atomic<int> x;
+    std::atomic<int> y;
+
+    for (int k = 0; k < g_nTries; ++k) {
+
+        std::thread producer{
+            [&a, &x, &y]() {
+                for (int j = 1; j < g_nSignals; ++j) {
+                    // TODO: Store
+                }
+            }
+        };
+        std::thread consumer{
+            [&a, &x, &y]() {
+                for (;;) {
+                    // TODO: Load
+                }
+            }
+        };
+
+        producer.join();
+        consumer.join();
+    }
 
 }
