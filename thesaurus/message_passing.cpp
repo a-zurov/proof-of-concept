dd3e375a7844e03ec00f397cf33298763054f010
a.zurov <a.zurov@s-e.su>
2023-03-24 17:48:05 +0200

    step 1

---
+++ thesaurus/message_passing.cpp
@@ -0,0 +1,12 @@
+// message_passing.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <thread>
+
+static const int g_nTries = 1, g_nSignals = 100;
+
+int main()
+{
+
+}
