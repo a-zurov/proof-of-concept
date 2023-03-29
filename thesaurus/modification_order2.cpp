9c2339f057c761f5f3a8f1722aba65fec1fff192
a.zurov <a.zurov@s-e.su>
2023-03-29 19:00:48 +0300

    experiment 2

--- thesaurus/modification_order2.cpp
+++ thesaurus/modification_order2.cpp
@@ -22,41 +22,51 @@ enum ID {
 std::atomic<bool> x, y;
 std::atomic<int> z;
 
-pair_t write_x(int j) {
+pair_t write_x(int& j) {
 
+    j = (int)id_write_x;
     x.store(true);
     return std::make_pair((int)id_write_x, j);
 }
 
-pair_t write_y(int j) {
+pair_t write_y(int& j) {
 
+    j = (int)id_write_y;
     y.store(true);
     return std::make_pair((int)id_write_y, j);
 }
 
-pair_t read_x_y(int j) {
+pair_t read_x_y(int& j) {
 
     while (!x.load()) {
         std::this_thread::yield();
     };
-    if (y.load()) ++z;
-    return std::make_pair((int)id_read_x_y, j);
+    int k = j;
+    if (y.load()) {
+        k = (k == j) ? j*10 : j;
+        ++z;
+    }
+    return std::make_pair((int)id_read_x_y, k);
 }
 
-pair_t read_y_x(int j) {
+pair_t read_y_x(int& j) {
 
     while (!y.load()) {
         std::this_thread::yield();
     };
-    if (x.load()) ++z;
-    return std::make_pair((int)id_read_y_x, j);
+    int k = j;
+    if (x.load()) {
+        k = (k == j) ? j*10 : j;
+        ++z;
+    }
+    return std::make_pair((int)id_read_y_x, k);
 }
 
 int main() {
 
     std::map<std::string, int> map_result;
 
-    std::vector<std::function<pair_t(int)>> vecCalls;
+    std::vector<std::function<pair_t(int&)>> vecCalls;
 
     vecCalls.push_back(write_x);
     vecCalls.push_back(write_y);
@@ -64,7 +74,7 @@ int main() {
     vecCalls.push_back(read_y_x);
 
     const int N = vecCalls.size();
-    const int M = 1000;
+    const int M = 1'000'000;
 
     ThreadPool pool(4);
 
@@ -77,9 +87,11 @@ int main() {
         z = 0;
         int offset = rand() % N;
 
+        int shared = 0;
+
         for (int j = 0; j < N; ++j) {
             results.emplace_back(
-                pool.enqueue(vecCalls[(offset + j) % 4], j)
+                pool.enqueue(vecCalls[(offset + j) % 4], std::ref(shared))
             );
         }
 
@@ -87,7 +99,7 @@ int main() {
         for (auto&& result : results) {
             try {
                 pair_t res = result.get();
-                str_result.append(std::to_string(res.first));
+                str_result.append('['+ std::to_string(res.first) + ',' + std::to_string(res.second)+']');
             }
             catch (std::runtime_error& ex) {
                 cout_dump_msg(ex.what());
@@ -105,4 +117,5 @@ int main() {
     for (const auto& [key, value] : map_result) {
         std::cout << key << ' ' << value << '\n';
     }
+    std::cout << "Total = " << map_result.size();
 }
