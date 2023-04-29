4f6b15bea55797f01b11de3304de0fa215c24c45
a.zurov <a.zurov@s-e.su>
2023-04-29 23:00:07 +0300

    asio example 21:00

--- asio-example/asio-example/asio_example.cpp
+++ asio-example/asio-example/asio_example.cpp
@@ -12,14 +12,48 @@
 #include <boost/asio/ts/buffer.hpp>
 #include <boost/asio/ts/internet.hpp>
 
+#define ASYNC_READ_ // <- async vs sync read switch
+#ifdef ASYNC_READ_
+#define SIZE_OF_CHUNK 1024
+
+std::vector<char> g_vecBuffer(SIZE_OF_CHUNK);
+
+void GrabSomeData(boost::asio::ip::tcp::socket& socket) {
+
+    socket.async_read_some( boost::asio::buffer(g_vecBuffer.data(), g_vecBuffer.size())
+                          , [&socket](boost::system::error_code ec, size_t length) {
+
+                                    std::cout << "\n\n" << "Async_read_some: " << length << " bytes\n\n";
+
+                                    if (!ec) {
+                                        for (size_t j = 0; j < length; ++j)
+                                            std::cout << g_vecBuffer[j];
+                                        GrabSomeData(socket);
+                                    }
+                                    else {
+                                        std::cout << "Async_read_some failed: " << ec.message() << '\n';
+                                    }
+                                }
+                          );
+}
+#endif // ASYNC_READ_
+
 
 int main() {
 
     boost::system::error_code ec;
 
+    boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::make_address("93.184.216.34"), 80);
+
     boost::asio::io_context context; // the platform specific interface
 
-    boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::make_address("93.184.216.34"), 80);
+#ifdef ASYNC_READ_
+
+    boost::asio::io_context::work idle(context);
+
+    std::thread context_thread{ [&context]() { context.run(); } };
+
+#endif // ASYNC_READ_
 
     boost::asio::ip::tcp::socket socket(context);
 
@@ -32,6 +66,10 @@ int main() {
 
     if (socket.is_open()) {
 
+#ifdef ASYNC_READ_
+        GrabSomeData(socket);
+#endif
+
         std::string szRequest =
             "GET /index.html HTTP/1.1\r\n"
             "Host: example.com\r\n"
@@ -39,10 +77,12 @@ int main() {
 
         socket.write_some(boost::asio::buffer(szRequest.data(), szRequest.size()), ec);
 
+#ifndef ASYNC_READ_
+
         socket.wait(socket.wait_read);
 
         size_t bytes = socket.available();
-        std::cout << "Socket has bytes availiable: " << bytes << '\n';
+        std::cout << "Socket has availiable " << bytes << "bytes\n";
 
         if (bytes > 0) {
             std::vector<char> vecBuffer(bytes);
@@ -53,5 +93,22 @@ int main() {
             }
             std::cout << std::endl;
         }
+#endif // NOT ASYNC_READ_
+
     }
+
+#ifdef ASYNC_READ_
+    using namespace std::chrono_literals;
+    std::this_thread::sleep_for(400ms);
+    context.stop();
+
+    if (context_thread.joinable()) {
+        try {
+            context_thread.join();
+        }
+        catch (std::system_error& ex) {
+            std::cout << ex.what();
+        }
+    }
+#endif // ASYNC_READ_
 }
