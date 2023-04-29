2e3bae5b5eb9a94fb1c73ebdaf4f22e4b86361f4
a.zurov <a.zurov@s-e.su>
2023-04-29 15:59:39 +0300

    asio example 9:30

--- asio-example/asio-example/asio_example.cpp
+++ asio-example/asio-example/asio_example.cpp
@@ -3,8 +3,30 @@
 // https://www.youtube.com/watch?v=2hNdkYInj4g
 
 #include <iostream>
+
+#ifdef _WIN32
+#define _WIN32_WINNT 0x0A00
+#endif
+
 #include <boost/asio.hpp>
+#include <boost/asio/ts/buffer.hpp>
+#include <boost/asio/ts/internet.hpp>
+
 
 int main() {
 
+    boost::system::error_code ec;
+
+    boost::asio::io_context context; // the platform specific interface
+
+    boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::make_address("93.184.216.34"), 80);
+
+    boost::asio::ip::tcp::socket socket(context);
+
+    socket.connect(endpoint, ec);
+
+    if(!ec)
+        std::cout << "Connection success!\n";
+    else
+        std::cout << "Connection failed: " << ec.message() << '\n';
 }
