1402cdec6e19d0f9bb880e7f0fe4174d4f078df9
a.zurov <a.zurov@s-e.su>
2023-04-29 13:42:42 +0300

    asio example 3:48

---
+++ asio-example/asio-example/asio_example.cpp
@@ -0,0 +1,9 @@
+// asio_example.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// --------------------------------------------------------------------------------------------------
+// https://www.youtube.com/watch?v=2hNdkYInj4g
+
+#include <iostream>
+
+int main() {
+
+}
