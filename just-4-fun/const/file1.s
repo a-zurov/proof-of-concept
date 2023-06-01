89bb9ce57d3b09865e43079cc14bdc026bf7b461
a.zurov <a.zurov@s-e.su>
2023-06-02 00:22:54 +0300

    just for fun 2

--- just-4-fun/const/file1.s
+++ just-4-fun/const/file1.s
@@ -3,5 +3,9 @@
 c1: .int 12
 
     .data
-    .global c2
+    .global c2, q1, q2, q3
 c2: .int 24
+q1: .quad 0x03, 0xDEADBEEF
+q2: .float
+q3: .double 2.2
+
--- just-4-fun/const/file2.c
+++ just-4-fun/const/file2.c
@@ -2,7 +2,9 @@
 
 extern const int c1;
 extern int c2;
+extern long long q1[];
 
 int main() {
     printf("%d %d\n", c1, ++c2);
+    printf("%llx %lld\n", q1[0], q1[1]);
 }
