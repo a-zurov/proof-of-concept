bf279b27fe567385f06248ef299781a462402053
a.zurov <a.zurov@s-e.su>
2023-06-02 15:20:59 +0300

    just for fun 3

--- just-4-fun/const/file1.s
+++ just-4-fun/const/file1.s
@@ -1,11 +1,30 @@
     .text
     .global c1
+    .align 4
 c1: .int 12
 
     .data
     .global c2, q1, q2, q3
+    .align 4
 c2: .int 24
+    .align 8
 q1: .quad 0x03, 0xDEADBEEF
+    .align 4
 q2: .float
+    .align 8
 q3: .double 2.2
 
+    .text
+    .global s1, ps1, pq3
+s1: .asciz "Hello, const!\n"
+    .align 8
+ps1:
+    .quad s1
+pq3:
+    .quad q3
+
+    .data
+    .global pq1_1
+    .align 8
+pq1_1:
+    .quad q1+8
--- just-4-fun/const/file2.c
+++ just-4-fun/const/file2.c
@@ -3,8 +3,25 @@
 extern const int c1;
 extern int c2;
 extern long long q1[];
+extern float q2;
+extern double q3;
+
+extern const char s1[];
+extern const char* const ps1;
+extern double * const pq3;
+extern long long * const pq1_1;
 
 int main() {
+
     printf("%d %d\n", c1, ++c2);
     printf("%llx %lld\n", q1[0], q1[1]);
+    printf("%p %p\n", q1, q1+1);
+    printf("%p %p\n", &q2, &q3);
+    printf("%f %f\n", q2, q3);
+    printf("%s", s1);
+    printf("%s", ps1);
+    printf("%f\n", *pq3);
+    printf("%p\n", pq3);
+    printf("%lld\n", *pq1_1);
+    printf("%p\n", pq1_1);
 }
