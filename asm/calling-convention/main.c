8e31bb8ecd72b889ffa6633684ae515ea82475cb
a.zurov <a.zurov@s-e.su>
2023-06-06 17:58:59 +0300

    minor changes

--- asm/calling-convention/main.c
+++ asm/calling-convention/main.c
@@ -8,6 +8,13 @@
 
 long long func1(void);
 void func3(void);
+int func4(int a, int b);
+
+struct Args {
+    int a;
+    int b;
+};
+int func5(struct Args x);
 
 #ifdef __cplusplus
     }
@@ -20,4 +27,11 @@ int main() {
     printf("%llx\n", res1);
 
     func3();
+
+    int res2 = func4(1234, 4747);
+    printf("%d\n", res2); // 5981
+
+    struct Args args = {1234, 4747};
+    int res3 = func5(args);
+    printf("%d\n", res3); // 5981
 }
