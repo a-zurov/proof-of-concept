12a645aaae8e137aed802443216f37b4b145deaa
a.zurov <a.zurov@s-e.su>
2023-06-14 22:51:13 +0300

    draft 1

--- drafts/interval-map/main.cpp
+++ drafts/interval-map/main.cpp
@@ -5,6 +5,8 @@
 #include <ostream>
 #include <type_traits>
 #include <map> // ??
+#include <optional>
+#include <memory>
 
 // For tests only:
 template< typename T
@@ -47,27 +49,54 @@ public:
 
 // Draft of interval_map datastructure:
 
-template<typename K, typename V>
+template< typename Key
+    , typename V
+    , typename Compare = std::less<Key>
+    >
 class interval_map {
 
-    std::map<K, V> map_; // ??
+    using map_t = std::map<Key, std::optional<V>, Compare >;
+
+    map_t map_;
 
 public:
 
-    // Assign value val to interval [keyBegin, keyEnd).
-    // Conforming to the C++ Standard Library conventions, the interval
-    // includes keyBegin, but excludes keyEnd.
-    // std::map has insert_or_assign method, which
-    // overwrite previous values in the interval.
-    void assign(const K& keyBegin, const K& keyEnd, const V& val) {
+    std::optional<V> operator[] (const Key& key) {
 
+        auto it = map_.upper_bound(key);
+        if (map_.begin() == it) {
+            return std::nullopt;
+        }
+        return (--it)->second;
+    }
+
+    void assign(const Key& keyBegin, const Key& keyEnd, const V& val) {
+
+        if (keyEnd <= keyBegin) {
+            return;
+        }
+        auto itEnd = map_.upper_bound(keyEnd);
+        if (map_.begin() == itEnd) {
+            map_.insert( std::pair(keyBegin, std::optional<V>(val)) );
+            map_.insert( std::pair(keyEnd, std::nullopt) );
+        }
     }
 };
 
 
 int main()
 {
+    /*
     CKey<int> key1;
     CKey key2{ 2 };
     std::cout << key1 << ' ' << key2;
+    */
+
+    interval_map<int, int> imap;
+    imap.assign(2, 4, 10);
+
+    for (int j = 0; j < 5; ++j) {
+        auto opt = imap[j];
+        std::cout << j << ':' << (opt.has_value() ? opt.value() : -1) << ' ';
+    }
 }
